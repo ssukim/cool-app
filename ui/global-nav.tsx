e4fb3dd090c3ccb4140421caa705f2134c308255
ssukim <tntntn0930@gmail.com>
2023-10-13 00:22:01 +0900

    feat: nav logo, mobile button

--- ui/global-nav.tsx
+++ ui/global-nav.tsx
@@ -2,6 +2,8 @@
 
 import Link from 'next/link';
 import { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import { NextLogo } from './next-logo';
 
 export function GlobalNav() {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,7 +17,7 @@ export function GlobalNav() {
           onClick={close}
         >
           <div className="h-7 w-7 rounded-full border border-white/30 group-hover:border-white/50">
-            logo
+            <NextLogo />
           </div>
 
           <h3 className="font-semibold tracking-wide text-gray-400 group-hover:text-gray-50">
@@ -23,6 +25,20 @@ export function GlobalNav() {
           </h3>
         </Link>
       </div>
+      <button
+        type="button"
+        className="group absolute right-0 top-0 flex h-14 items-center gap-x-2 px-4 lg:hidden"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className="font-medium text-gray-100 group-hover:text-gray-400">
+          Menu
+        </div>
+        {isOpen ? (
+          <AiOutlineClose className="text-gray-400" size={25} />
+        ) : (
+          <AiOutlineMenu className="text-gray-400" size={25} />
+        )}
+      </button>
     </div>
   );
 }
